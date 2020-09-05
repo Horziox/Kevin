82724b74e3b0f5df11481f33a538b8681efd51b5
Horziox <horziox.dev@gmail.com>
2020-09-05 15:14:56 +0200

    Add dev Tag in bot info command

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -11,6 +11,7 @@ module.exports = {
         .addField("Ping", `Bot : ${Date.now() - message.createdTimestamp}ms\nDiscord : ${bot.ws.ping}ms`, true)
         .addField("Bot Stats", `${bot.guilds.cache.size} serveurs\n${bot.users.cache.size} utilisateurs\n${bot.channels.cache.size} salons`, true)
         .addField("Liens Utiles", "[Invitez moi !](https://discord.com/oauth2/authorize?client_id=739849791168577608&scope=bot&permissions=322624)\n[Rejoignez mon serveur de support !](https://discord.gg/7XyNM4p)")
+        .addField("Développeur", bot.users.cache.get("340212760870649866").tag, true)
         .setColor("#bf9322")
         .setTimestamp()
         .setFooter(`Commande faite par ${message.author.username}`, message.author.displayAvatarURL({dynamic: true}))
